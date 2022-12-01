1b43002d979fd0289d3da13d7e1790a25e3e0aa5
Ankitha . <Ankitha.Ankitha1@ibm.com>
2022-12-01 16:00:30 +0550

    Initial Source code

---
+++ README.md
@@ -0,0 +1,14 @@
+**Open Service Broker API Specifications for Java:**
+
+| S.No | Task Name                                                     | File Name | Function Name |
+|------|---------------------------------------------------------------|-----------|---------------|
+| 1    | Get a Service instance                                        |           |               |
+| 2    | Provisioning new service instance                             |           |               |
+| 3    | Deprovisioning service instance                               |           |               |
+| 4    | Update a service instance                                     |           |               |
+| 5    | Get the latest requested operation state for service instance |           |               |
+| 6    | Get a service binding                                         |           |               |
+| 7    | Generate a service binding                                    |           |               |
+| 8    | Deprovision a service binding                                 |           |               |
+| 9    | Get the latest requested operation state for service binding  |           |               |
+| 10   | Get Catalog                                                   |           |               |
---
+++ service/CatalogService.js
@@ -0,0 +1,139 @@
+'use strict';
+
+
+/**
+ * get the catalog of services that the service broker offers
+ *
+ * xBrokerAPIVersion String version number of the Service Broker API that the Platform will use
+ * returns Catalog
+ **/
+exports.catalogGetUsingGET = function(xBrokerAPIVersion) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+  "services" : [ {
+    "bindable" : false,
+    "metadata" : { },
+    "plan_updateable" : false,
+    "plans" : [ {
+      "bindable" : false,
+      "maintenance_info" : {
+        "description" : "description",
+        "version" : "version"
+      },
+      "plan_updateable" : false,
+      "schemas" : {
+        "service_binding" : {
+          "create" : {
+            "parameters" : { }
+          }
+        },
+        "service_instance" : { }
+      },
+      "binding_rotatable" : false,
+      "name" : "name",
+      "description" : "description",
+      "id" : "id",
+      "free" : false,
+      "maximum_polling_duration" : 0
+    }, {
+      "bindable" : false,
+      "maintenance_info" : {
+        "description" : "description",
+        "version" : "version"
+      },
+      "plan_updateable" : false,
+      "schemas" : {
+        "service_binding" : {
+          "create" : {
+            "parameters" : { }
+          }
+        },
+        "service_instance" : { }
+      },
+      "binding_rotatable" : false,
+      "name" : "name",
+      "description" : "description",
+      "id" : "id",
+      "free" : false,
+      "maximum_polling_duration" : 0
+    } ],
+    "binding_rotatable" : false,
+    "name" : "name",
+    "description" : "description",
+    "id" : "id",
+    "dashboard_client" : {
+      "id" : "id",
+      "redirect_uri" : "redirect_uri",
+      "secret" : "secret"
+    },
+    "requires" : [ "route_forwarding", "route_forwarding" ],
+    "tags" : [ "tags", "tags" ]
+  }, {
+    "bindable" : false,
+    "metadata" : { },
+    "plan_updateable" : false,
+    "plans" : [ {
+      "bindable" : false,
+      "maintenance_info" : {
+        "description" : "description",
+        "version" : "version"
+      },
+      "plan_updateable" : false,
+      "schemas" : {
+        "service_binding" : {
+          "create" : {
+            "parameters" : { }
+          }
+        },
+        "service_instance" : { }
+      },
+      "binding_rotatable" : false,
+      "name" : "name",
+      "description" : "description",
+      "id" : "id",
+      "free" : false,
+      "maximum_polling_duration" : 0
+    }, {
+      "bindable" : false,
+      "maintenance_info" : {
+        "description" : "description",
+        "version" : "version"
+      },
+      "plan_updateable" : false,
+      "schemas" : {
+        "service_binding" : {
+          "create" : {
+            "parameters" : { }
+          }
+        },
+        "service_instance" : { }
+      },
+      "binding_rotatable" : false,
+      "name" : "name",
+      "description" : "description",
+      "id" : "id",
+      "free" : false,
+      "maximum_polling_duration" : 0
+    } ],
+    "binding_rotatable" : false,
+    "name" : "name",
+    "description" : "description",
+    "id" : "id",
+    "dashboard_client" : {
+      "id" : "id",
+      "redirect_uri" : "redirect_uri",
+      "secret" : "secret"
+    },
+    "requires" : [ "route_forwarding", "route_forwarding" ],
+    "tags" : [ "tags", "tags" ]
+  } ]
+};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
